5aaa71bced06bade52b64dbc921f9fa228f112ee
byf1n <kirillozubenko@gmail.com>
2022-10-06 00:20:07 +0200

    hm3

---
+++ src/services/users.service.js
@@ -0,0 +1,8 @@
+import {axiosService} from "./axios.service";
+import {urls} from "../config";
+
+const usersService = {
+    getAll: () => axiosService.get(urls.users),
+    getPosts: (userId) => axiosService.get(`${urls.posts}/?userId=${userId}`)
+}
+
